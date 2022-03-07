0c8df40f223fb0e203d113d4bcf339fecb62e11c
DMH695 <2313978908@qq.com>
2022-03-07 15:50:40 +0800

    build: 项目初始化

---
+++ src/main/java/com/example/recoverbysporting/RecoverBySportingApplication.java
@@ -0,0 +1,12 @@
+package com.example.recoverbysporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecoverBySportingApplication {
+        public static void main(String[] args) {
+            SpringApplication.run(RecoverBySportingApplication.class, args);
+        }
+
+}
